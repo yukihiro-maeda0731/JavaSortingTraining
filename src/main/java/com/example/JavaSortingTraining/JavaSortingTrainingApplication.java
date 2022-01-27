3726d63a610498f79fcbcc91761068c26663866a
前田 <sloth19920731@gmail.com>
2022-01-27 23:35:59 +0900

    springpj作成

---
+++ src/main/java/com/example/JavaSortingTraining/JavaSortingTrainingApplication.java
@@ -0,0 +1,14 @@
+package com.example.JavaSortingTraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSortingTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSortingTrainingApplication.class, args);
+		System.out.print("Hello");
+	}
+
+}
---
+++ src/test/java/com/example/JavaSortingTraining/JavaSortingTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JavaSortingTraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSortingTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
