a0e1fc2124c3253183d65d4765ddf76180628d9f
前田 <sloth19920731@gmail.com>
2022-01-27 23:46:52 +0900

    比較対象のモデル作成

--- src/main/java/com/example/JavaSortingTraining/JavaSortingTrainingApplication.java
+++ src/main/java/com/example/JavaSortingTraining/JavaSortingTrainingApplication.java
@@ -1,14 +1,26 @@
 package com.example.JavaSortingTraining;
 
+import com.example.JavaSortingTraining.model.Pokemon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class JavaSortingTrainingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaSortingTrainingApplication.class, args);
-		System.out.print("Hello");
+		List<Pokemon> pokemonList = new ArrayList<Pokemon>();
+		Pokemon yadoking = new Pokemon("yadoking",220,0);
+		Pokemon mugenDaina = new Pokemon("mugen-daina",340,270);
+		Pokemon zigzagma = new Pokemon("zigzagma",60,30);
+		Pokemon blucky = new Pokemon("blucky",310,170);
+		Pokemon fireBird = new Pokemon("fireBird",220,190);
+		pokemonList.add(mugenDaina);
+		pokemonList.add(blucky);
+		System.out.print(pokemonList);
 	}
 
 }
---
+++ src/main/java/com/example/JavaSortingTraining/model/Pokemon.java
@@ -0,0 +1,14 @@
+package com.example.JavaSortingTraining.model;
+
+import com.sun.jdi.IntegerValue;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Pokemon {
+    private String name;
+    private Integer HP;
+    private Integer maxAttackPoint;
+
+}
