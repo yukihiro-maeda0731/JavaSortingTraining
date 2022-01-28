fd8b662d0e44ea35b9440e37ebc871fb3cf570e1
前田 <sloth19920731@gmail.com>
2022-01-28 23:52:45 +0900

    Comparator作成

--- src/main/java/com/example/JavaSortingTraining/JavaSortingTrainingApplication.java
+++ src/main/java/com/example/JavaSortingTraining/JavaSortingTrainingApplication.java
@@ -1,11 +1,14 @@
 package com.example.JavaSortingTraining;
 
 import com.example.JavaSortingTraining.model.Pokemon;
+//import com.example.JavaSortingTraining.model.Trainer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class JavaSortingTrainingApplication {
@@ -18,9 +21,19 @@ public class JavaSortingTrainingApplication {
 		Pokemon zigzagma = new Pokemon("zigzagma",60,30);
 		Pokemon blucky = new Pokemon("blucky",310,170);
 		Pokemon fireBird = new Pokemon("fireBird",220,190);
+		pokemonList.add(zigzagma);
 		pokemonList.add(mugenDaina);
 		pokemonList.add(blucky);
-		System.out.print(pokemonList);
+
+		Comparator<Pokemon> hpComparator = Comparator.comparing(Pokemon::getHP);
+		//逆にしたい時は.reversed()
+		Comparator<Pokemon> reverseHPComparator = Comparator.comparing(Pokemon::getHP).reversed();
+
+		List<Pokemon> sortedPokemonsByHP = pokemonList.stream()
+				// ここで比較ルール設定
+				.sorted(reverseHPComparator)
+				.collect(Collectors.toList());
+		System.out.print(sortedPokemonsByHP);
 	}
 
 }
--- src/main/java/com/example/JavaSortingTraining/model/Pokemon.java
+++ src/main/java/com/example/JavaSortingTraining/model/Pokemon.java
@@ -3,6 +3,7 @@ package com.example.JavaSortingTraining.model;
 import com.sun.jdi.IntegerValue;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
@@ -10,5 +11,15 @@ public class Pokemon {
     private String name;
     private Integer HP;
     private Integer maxAttackPoint;
+//    private Trainer trainer;
 
 }
+
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class Trainer {
+//    private String tool;
+//    private String name;
+//    private String birthDate;
+//}
