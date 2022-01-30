18153d91ba0be8512023fbd6121c945acf1a6acb
前田 <sloth19920731@gmail.com>
2022-01-30 23:44:33 +0900

    responseEntityのgetBodyで取ってきた値を独自モデルクラスに変換して操作(失敗)

--- src/main/java/com/example/JavaSortingTraining/model/Pokemon.java
+++ src/main/java/com/example/JavaSortingTraining/model/Pokemon.java
@@ -5,15 +5,38 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 public class Pokemon {
-    private String name;
-    private Integer HP;
-    private Integer maxAttackPoint;
-//    private Trainer trainer;
+    private Abilities abilities;
 
 }
+
+@Data
+@AllArgsConstructor
+class Abilities {
+    private List<Ability> ability;
+}
+
+@Data
+@AllArgsConstructor
+class Ability {
+    private String name;
+    private String url;
+    private boolean is_hidden;
+    private int slot;
+}
+
+
+//public class Pokemon {
+//    private String name;
+//    private Integer HP;
+//    private Integer maxAttackPoint;
+//    private Trainer trainer;
+
+//}
 
 //@Data
 //@AllArgsConstructor
