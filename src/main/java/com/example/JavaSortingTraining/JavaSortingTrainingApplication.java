fb2047f1ceca4175d2084d3f898d842500b0b9b2
前田 <sloth19920731@gmail.com>
2022-02-01 23:25:10 +0900

    @Builder使ってモデル実装

--- src/main/java/com/example/JavaSortingTraining/JavaSortingTrainingApplication.java
+++ src/main/java/com/example/JavaSortingTraining/JavaSortingTrainingApplication.java
@@ -19,37 +19,44 @@ public class JavaSortingTrainingApplication {
 
 	public static void main(String[] args) {
 
-		RestTemplate rest = new RestTemplate();
-		String url = "https://pokeapi.co/api/v2/pokemon/pikachu/";
+//		RestTemplate rest = new RestTemplate();
+//		String url = "https://pokeapi.co/api/v2/pokemon/pikachu/";
+//
+//		ResponseEntity<Object> response = rest.exchange(url,  HttpMethod.GET,
+//				null, new ParameterizedTypeReference<>() {
+//				});
+//
+//		Object json =  response.getBody();
+//
+//		System.out.println(json);
 
-		ResponseEntity<Object> response = rest.exchange(url,  HttpMethod.GET,
-				null, new ParameterizedTypeReference<>() {
-				});
-
-		Object json =  response.getBody();
-
-		System.out.println(json);
-
-//		SpringApplication.run(JavaSortingTrainingApplication.class, args);
-//		List<Pokemon> pokemonList = new ArrayList<Pokemon>();
+		SpringApplication.run(JavaSortingTrainingApplication.class, args);
+		List<Pokemon> pokemonList = new ArrayList<Pokemon>();
 //		Pokemon yadoking = new Pokemon("yadoking",220,0);
 //		Pokemon mugenDaina = new Pokemon("mugen-daina",340,270);
 //		Pokemon zigzagma = new Pokemon("zigzagma",60,30);
 //		Pokemon blucky = new Pokemon("blucky",310,170);
 //		Pokemon fireBird = new Pokemon("fireBird",220,190);
-//		pokemonList.add(zigzagma);
-//		pokemonList.add(mugenDaina);
-//		pokemonList.add(blucky);
-//
-//		Comparator<Pokemon> hpComparator = Comparator.comparing(Pokemon::getHP);
-//		//逆にしたい時は.reversed()
+
+		Pokemon yadoking = Pokemon.builder().name("yadoking").HP(220).maxAttackPoint(0).build();
+		Pokemon mugenDaina = new Pokemon("mugen-daina",340,270);
+		Pokemon zigzagma = new Pokemon("zigzagma",60,30);
+		Pokemon blucky = new Pokemon("blucky",310,170);
+		Pokemon fireBird = new Pokemon("fireBird",220,190);
+		pokemonList.add(yadoking);
+		pokemonList.add(zigzagma);
+		pokemonList.add(mugenDaina);
+		pokemonList.add(blucky);
+
+		Comparator<Pokemon> hpComparator = Comparator.comparing(Pokemon::getHP);
+		//逆にしたい時は.reversed()
 //		Comparator<Pokemon> reverseHPComparator = Comparator.comparing(Pokemon::getHP).reversed();
-//
-//		List<Pokemon> sortedPokemonsByHP = pokemonList.stream()
-//				// ここで比較ルール設定
-//				.sorted(reverseHPComparator)
-//				.collect(Collectors.toList());
-//		System.out.print(sortedPokemonsByHP);
+
+		List<Pokemon> sortedPokemonsByHP = pokemonList.stream()
+				// ここで比較ルール設定
+				.sorted(hpComparator)
+				.collect(Collectors.toList());
+		System.out.print(sortedPokemonsByHP);
 	}
 
 }
--- src/main/java/com/example/JavaSortingTraining/model/Pokemon.java
+++ src/main/java/com/example/JavaSortingTraining/model/Pokemon.java
@@ -2,6 +2,7 @@ package com.example.JavaSortingTraining.model;
 
 import com.sun.jdi.IntegerValue;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,40 +10,9 @@ import java.util.List;
 
 @Data
 @AllArgsConstructor
+@Builder
 public class Pokemon {
-    private Abilities abilities;
-
-}
-
-@Data
-@AllArgsConstructor
-class Abilities {
-    private List<Ability> ability;
-}
-
-@Data
-@AllArgsConstructor
-class Ability {
     private String name;
-    private String url;
-    private boolean is_hidden;
-    private int slot;
+    private Integer HP;
+    private Integer maxAttackPoint;
 }
-
-
-//public class Pokemon {
-//    private String name;
-//    private Integer HP;
-//    private Integer maxAttackPoint;
-//    private Trainer trainer;
-
-//}
-
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
-//public class Trainer {
-//    private String tool;
-//    private String name;
-//    private String birthDate;
-//}
